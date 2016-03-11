5be752a23bf98fdb7f28b81bb0fd2195dbc645ca
Allen Park <allenpark@mit.edu>
2016-03-11 13:31:21 -0500

    Love Letter, Cockroach Poker, One Night, 6 Nimmt, The Game, No Thanks

--- README.md
+++ README.md
@@ -2,7 +2,24 @@
 
 This is a web page with 108 easily printable cards with different game rules. The current labels are:
 
-- Numbering from 1 to 108
-- Two decks of playing cards plus four jokers
-- Hanabi
-- Seventh Hero
+- Numbering from 1 to 108 (1-108)
+- Two decks of playing cards plus four jokers (1-108)
+- Hanabi (1-60)
+- Seventh Hero (1-77)
+- No Thanks! [with default numbering] (3-35)
+- The Game: Are you ready to play? [with default numbering] (2-99)
+- One Night Ultimate Werewolf (61-76)
+- Cockroach Poker (1-64)
+- 6 nimmt! (1-104)
+- Love Letter (78 - 93)
+
+Here is the list of games that can possibly be added:
+
+- One Night Ultimate Werewolf: Daybreak (11)
+- SET (81)
+- Resistance (15)
+- Coup (15)
+- Red7 (49)
+- Sushi Go! (108)
+- Code Names (108)
+- Citadels
--- cards.js
+++ cards.js
@@ -38,7 +38,7 @@ $(document).ready(function() {
 
     if (1 <= i && i <= 77) {
       var names = ["Shield Maiden", "Wyrm Slayer", "Chieftain", "Outcast", "Oracle", "Tale Weaver", "Forest Strider"];
-      var abilities = ["Nullify an ability as an interrupt.", "Discard the wandering hero.", "Renew one of your hero's abilities.", "Another player of your choice cannot recruit the wandering hero.", "Look at the wandering hero.", "Main player may send any hero.", "Draw two cards into your hand."];
+      var abilities = ["Nullify an ability as an interrupt.", "Discard the wandering hero.", "Renew one of your hero's abilities.", "Block someone from recruiting.", "Look at the wandering hero.", "Main player may send any hero.", "Draw two cards into your hand."];
       var needed = ["Number 3 or lower", "Number 5 or higher", "Number 3-5", "Odd number", "Even number", "Number of players or lower", "Number of players or higher"];
       var num = Math.floor((i-1) / 11) + 1;
       var seventhhero = '<div class="seventhhero inner"><div class="text">';
@@ -48,6 +48,74 @@ $(document).ready(function() {
       seventhhero += "</div></div>";
       inner += seventhhero;
     }
+
+    if (1 <= i && i <= 64) {
+      var bugs = ["Cockroach", "Bat", "Fly", "Toad", "Rat", "Scorpion", "Spider", "Stink Bug"];
+      var bug = bugs[Math.floor((i-1) / 8)];
+      var cockroachpoker = '<div class="cockroachpoker inner"><div class="text">';
+      cockroachpoker += "<b>Cockroach Poker</b>"
+      cockroachpoker += '<span class="spacing">' + bug + "</span>";
+      cockroachpoker += "</div></div>";
+      inner += cockroachpoker;
+    }
+
+    if (61 <= i && i <= 76) {
+      var players = ["Werewolf", "Werewolf", "Villager", "Villager", "Villager", "Seer", "Robber", "Insomniac", "Troublemaker", "Minion", "Doppelg&auml;nger", "Mason", "Mason", "Drunk", "Tanner", "Hunter"];
+      var player = players[i - 61];
+      var onenight = '<div class="onenight inner"><div class="text">';
+      onenight += "<b>One Night</b>";
+      onenight += '<span class="spacing">' + player + "</span>";
+      onenight += "</div></div>";
+      inner += onenight;
+    }
+
+    if (1 <= i && i <= 104) {
+      var cattle = 1;
+      if (i % 10 == 5) {
+        cattle = 2;
+      }
+      if (i % 10 == 0) {
+        cattle = 3;
+      }
+      if (i % 11 == 0) {
+        cattle = 5;
+      }
+      if (i == 55) {
+        cattle = 7;
+      }
+      var nimmt = '<div class="nimmt inner"><div class="text">';
+      nimmt += "<b>6 Nimmt!</b>";
+      nimmt += '<span class="spacing">' + cattle + " cattle</span>";
+      nimmt += "</div></div>";
+      inner += nimmt;
+    }
+
+    if (78 <= i && i <= 93) {
+      var nums = [1, 1, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 8];
+      var names = ["Guard", "Priest", "Baron", "Handmaid", "Prince", "King", "Countess", "Princess"];
+      var abilities = ["Correctly guess another player's non-Guard card to get them out.", "Look at another player's hand.<br/>&nbsp;", "Secretly compare hands. The player with the lower value is out.", "Until your next turn, ignore all effects from other player's cards.", "Choose a player to discard their hand and draw a new card.", "Trade hands with another player.<br/>&nbsp;", "Discard this card if you have the King or Prince.", "If you discard this card, you are out of the round."];
+      var index = i - 78;
+      var num = nums[index];
+      var name = names[num - 1];
+      var ability = abilities[num - 1];
+      var loveletter = '<div class="loveletter inner"><div class="text">';
+      loveletter += "<b>Love Letter</b> ";
+      loveletter += '<span class="spacing">' + num + " " + name + '</span><br/>';
+      loveletter += ability;
+      inner += loveletter;
+    }
+
+    var games = [];
+    if (2 <= i && i <= 99) {
+      games.push("<b>The Game</b>");
+    }
+    if (3 <= i && i <= 35) {
+      games.push("<b>No Thanks!</b>");
+    }
+    var others = '<div class="others inner"><div class="text">';
+    others += games.join(" + ") + "&nbsp;";
+    others += "</div></div>";
+    inner += others;
 
     $("body").append('<div class="card">' + inner + '</div>');
   }
